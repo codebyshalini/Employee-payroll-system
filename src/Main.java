a5f0013cf2ec952fa4ae9dc7b919e1bfa847f6ad
shalinibycode <shaliniyadav09760@gmail.com>
2025-09-24 14:43:38 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,110 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Employee {
+    private String name;
+    private int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // Abstract method to be implemented by subclasses
+    public abstract double calculateSalary();
+
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", id=" + id + ", salary=" + calculateSalary() + "]";
+    }
+}
+
+class FullTimeEmployee extends Employee {
+    private double monthlySalary;
+
+    public FullTimeEmployee(String name, int id, double monthlySalary) {
+        super(name, id);
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return monthlySalary;
+    }
+}
+
+class PartTimeEmployee extends Employee {
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate) {
+        super(name, id);
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+}
+
+class PayrollSystem {
+    private List<Employee> employeeList;
+
+    public PayrollSystem() {
+        employeeList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public void removeEmployee(int id) {
+        Employee employeeToRemove = null;
+        for (Employee employee : employeeList) {
+            if (employee.getId() == id) {
+                employeeToRemove = employee;
+                break;
+            }
+        }
+        if (employeeToRemove != null) {
+            employeeList.remove(employeeToRemove);
+        }
+    }
+
+    public void displayEmployees() {
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        PayrollSystem payrollSystem = new PayrollSystem();
 
+        FullTimeEmployee emp1 = new FullTimeEmployee("John Doe", 101, 5000.0);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Jane Smith", 102, 30, 15.0);
 
+        payrollSystem.addEmployee(emp1);
+        payrollSystem.addEmployee(emp2);
+
+        System.out.println("Initial Employee Details:");
+        payrollSystem.displayEmployees();
+
+        System.out.println("\nRemoving Employee...");
+        payrollSystem.removeEmployee(101);
+
+        System.out.println("\nRemaining Employee Details:");
+        payrollSystem.displayEmployees();
     }
 }
